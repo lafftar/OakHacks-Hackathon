d40a50e7b04214d5af2076ad250f34aff2aeeb09
lafftar <tibabalase@gmail.com>
2020-09-12 13:05:21 -0400

    Implemented writing and reading data to db, working on time matching

--- backend/base.py
+++ backend/base.py
@@ -3,6 +3,8 @@ import yfinance
 from backend.db_interface import create_connection
 from sqlalchemy import create_engine
 import pandas as pd
+from pytz import timezone
+from datetime import datetime
 
 API_KEY = "l6W2qcWFlQIvj7wVbkQu4t9qo"
 API_KEY_SECRET = "NCu9kQIM8E0n16e1NBXVSFBbeq0ovcG9pQuo2oerNDNXg327To"
@@ -18,13 +20,33 @@ api = twitter.Api(consumer_key=API_KEY,
                   access_token_secret=ACCESS_TOKEN_SECRET)
 
 
-# item = api.GetUserTimeline(screen_name="elonmusk")
-#
-# for tem in item:
-#     print(tem)
-# create_connection()
-stock = yfinance.Ticker('tsla')
-data_frame = pd.DataFrame(stock.history(period="1d", actions=False, interval="1m"))
+items = api.GetUserTimeline(screen_name="elonmusk")
+print(items[3])
+eastern = timezone('US/Eastern')
+utc = timezone('UTC')
+
+created_at = datetime.strptime(items[0].created_at, '%a %b %d %H:%M:%S +0000 %Y')
+utc_created_at = utc.localize(created_at)
+print(created_at)
+print(utc_created_at)
+
 # db_connection = create_engine('sqlite:///base.db')
-# data_frame.to_sql(f"elonmusk", if_exists='replace', method='multi', con=db_connection)
-print(data_frame)
+#
+# tweet_dict = {
+#     "Created At": [],
+#     "Tweet Text": []
+# }
+# for item in items:
+#     tweet_dict["Created At"].append(item.created_at)
+#     tweet_dict["Tweet Text"].append(item.text)
+#
+# print(tweet_dict)
+# data_frame = pd.DataFrame(tweet_dict)
+# print(data_frame)
+# data_frame.to_sql(f"elonmusk_tweets", if_exists='replace', method='multi', con=db_connection, chunksize=100, index=False)
+# create_connection()
+# stock = yfinance.Ticker('tsla')
+# # data_frame = pd.DataFrame(stock.history(period="1d", actions=False, interval="1m"))
+# # data_frame.to_sql(f"elonmusk_fin_data", if_exists='replace', method='multi', con=db_connection, chunksize=100, index=False)
+# data_frame = pd.read_sql_table('elonmusk_fin_data', con=db_connection)
+# print(data_frame)
--- backend/db_interface.py
+++ backend/db_interface.py
@@ -8,10 +8,20 @@ def create_connection():
     return conn
 
 
-def create_table(table_name):
+def cursor():
+    conn = create_connection()
+    return conn.cursor()
+
+
+def create_fin_data_table(table_name):
     create_table_sql = f"""CREATE TABLE {table_name}(
-                            page_number integer PRIMARY KEY, 
-                            api_data text,
-                            date_time text
+                            date_time date PRIMARY KEY, 
+                            open int,
+                            high int,
+                            low int,
+                            close int,
+                            volume int
                         );"""
     cursor().execute(create_table_sql)
+
+
