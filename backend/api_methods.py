97912653658b7666a07b07a39b2318025eb22652
lafftar <tibabalase@gmail.com>
2020-09-13 04:05:20 -0400

    Changed the way the backend outputs json

--- backend/api_methods.py
+++ backend/api_methods.py
@@ -1,18 +1,33 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from flask import jsonify
 
 db_connection = create_engine('sqlite:///../db/base.db')
 
 
 def get_fin_data(username):
     data_frame = pd.read_sql_table(f'{username}_fin_data', con=db_connection)
-    data_frame = data_frame.to_json()
-    return data_frame
+    data_frame = data_frame[["Datetime", "Close"]]
+    data_frame.reset_index(drop=True, inplace=True)
+    output = []
+    for item in data_frame.values:
+        output.append({
+            "DateTime": item[0],
+            "Close": item[1]
+        })
+    return output
 
 
 def get_tweets(username):
     data_frame = pd.read_sql_table(f'{username}_tweets', con=db_connection)
-    data_frame = data_frame.to_json()
-    return data_frame
+    data_frame.reset_index(drop=True, inplace=True)
+    output = []
+    for item in data_frame.values:
+        output.append({
+            "CreatedAt": item[0],
+            "TweetLink": item[1],
+            "TweetText": item[2]
+        })
+    return output
 
 # print(get_tweets('elonmusk'))
